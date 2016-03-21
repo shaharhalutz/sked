5da36de8e9825c695c0a7ec8708a1e8518d51ab8
shaharhalutz <shahar.halutz@gmail.com>
2016-03-21 11:18:32 +0000

    enabled remove from favorites on click

--- server/methods.js
+++ server/methods.js
@@ -5,9 +5,14 @@ Meteor.methods({
     }
 
     if (_(Meteor.user().profile.votedProductIds).include(_id)) {
+      
+      // remove:
+      Products.update({_id: _id}, {$inc: {numberOfVotes: -1}, $pull: {voterIds: this.userId}});
+      Meteor.users.update({_id: this.userId}, {$pull: {'profile.votedProductIds': _id}});
       return;
     }
-
+    
+    // add:
     Products.update({_id: _id}, {$inc: {numberOfVotes: 1}, $addToSet: {voterIds: this.userId}});
     Meteor.users.update({_id: this.userId}, {$addToSet: {'profile.votedProductIds': _id}});
   }
